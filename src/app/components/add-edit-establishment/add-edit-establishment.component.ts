e5ab2e2b1e6cc3ad04d20ed80026219d8acd3cd4
Jan Olaerts <jan.olaerts92@gmail.com>
2020-10-26 16:37:43 +0100

    able to put parking checkbox next to label

--- src/app/components/add-edit-establishment/add-edit-establishment.component.ts
+++ src/app/components/add-edit-establishment/add-edit-establishment.component.ts
@@ -31,9 +31,26 @@ export class AddEditEstablishmentComponent implements OnInit {
         zipcode: new FormControl('', [Validators.required, Validators.pattern(new RegExp(/^\\d{4}$/))]),
         community: new FormControl('', [Validators.required, Validators.minLength(2)])
       }),
-      openingHours: this.formBuilder.group({}),
-      menu: this.formBuilder.group({}),
-      tables: this.formBuilder.group({}),
+      openingHours: this.formBuilder.group({
+        openingsuurMa: new FormControl('Openingsuur:', [JammikValidators.valueCannotBeOpeningsuur]),
+        sluitingsuurMa: new FormControl('Sluitingsuur:', [JammikValidators.valueCannotBeSluitingsuur]),
+        openingsuurDi: new FormControl('Openingsuur:', [JammikValidators.valueCannotBeOpeningsuur]),
+        sluitingsuurDi: new FormControl('Sluitingsuur:', [JammikValidators.valueCannotBeSluitingsuur]),
+        openingsuurWo: new FormControl('Openingsuur:', [JammikValidators.valueCannotBeOpeningsuur]),
+        sluitingsuurWo: new FormControl('Sluitingsuur:', [JammikValidators.valueCannotBeSluitingsuur]),
+        openingsuurDo: new FormControl('Openingsuur:', [JammikValidators.valueCannotBeOpeningsuur]),
+        sluitingsuurDo: new FormControl('Sluitingsuur:', [JammikValidators.valueCannotBeSluitingsuur]),
+        openingsuurVr: new FormControl('Openingsuur:', [JammikValidators.valueCannotBeOpeningsuur]),
+        sluitingsuurVr: new FormControl('Sluitingsuur:', [JammikValidators.valueCannotBeSluitingsuur]),
+        openingsuurZa: new FormControl('Openingsuur:', [JammikValidators.valueCannotBeOpeningsuur]),
+        sluitingsuurZa: new FormControl('Sluitingsuur:', [JammikValidators.valueCannotBeSluitingsuur]),
+        openingsuurZo: new FormControl('Openingsuur:', [JammikValidators.valueCannotBeOpeningsuur]),
+        sluitingsuurZo: new FormControl('Sluitingsuur:', [JammikValidators.valueCannotBeSluitingsuur])
+      }),
+      tables: this.formBuilder.group({
+        aantalTafels: new FormControl('', [Validators.required, Validators.min(1)]),
+        aantalStoelen: new FormControl('', [Validators.required, Validators.min(1)])
+      }),
     });
   }
 
--- src/app/validators/jammik-validators.ts
+++ src/app/validators/jammik-validators.ts
@@ -4,12 +4,30 @@ export class JammikValidators {
 
   static notOnlyWhitespace(control: FormControl): ValidationErrors {
 
-    if(control.value != null && control.value.trim().length === 0) {
+    if (control.value != null && control.value.trim().length === 0) {
 
       return { 'notOnlyWhitespace': true };
     } else {
 
       return null;
     }
+  }
+
+  static valueCannotBeOpeningsuur(control: FormControl): ValidationErrors {
+
+    if (control.value != null && control.value === 'Openingsuur:')
+      return { 'valueCannotBeOpeningsuur': true };
+
+    else
+      return null;
+  }
+
+  static valueCannotBeSluitingsuur(control: FormControl): ValidationErrors {
+
+    if (control.value != null && control.value === 'Sluitingsuur:')
+    return { 'valueCannotBeSluitingsuur': true };
+
+  else
+    return null;
   }
 }
