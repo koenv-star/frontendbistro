090544b486d288e42fc47fb2752a563ee020f6ee
michael creelle <michael.creelle@telenet.be>
2020-11-01 12:47:13 +0100

    removed exces code added isGeslote from dag to zaakcomponent

--- src/app/models/dag.ts
+++ src/app/models/dag.ts
@@ -5,8 +5,4 @@ export class Dag {
   naam: string;
   openingsUur: Time;
   sluitingsUur: Time;
-
-  public isGesloten():boolean {
-    return this.openingsUur == null && this.sluitingsUur == null;
-  }
 }
