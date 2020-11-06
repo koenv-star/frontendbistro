6d9b85ec0d5578a2d8a162e014bb3b719fc33e0b
koen vochten <koen.voc@hotmail.com>
2020-11-06 14:08:08 +0100

    savety commit

--- src/app/components/reservaties/reservaties.component.ts
+++ src/app/components/reservaties/reservaties.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 import { Reservatie } from 'src/app/models/reservatie';
 import { Zaak } from 'src/app/models/zaak';
 import { ReservatieService } from 'src/app/services/reservatie.service';
@@ -14,9 +15,10 @@ import { ZaakService } from 'src/app/services/zaak.service';
 export class ReservatiesComponent implements OnInit {
   reservaties: Reservatie[];
   id: number;
-  zaaknaam: String;
   zaak: Zaak;
   reservatieForm: FormGroup;
+  tafels: Map<number, number>;
+
   constructor(
     private route: ActivatedRoute,
     private reservatiesservice: ReservatieService,
@@ -24,6 +26,10 @@ export class ReservatiesComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    console.log(this.id);
+    this.getReservatiesByZaakId(this.id);
+    this.getZaak();
     this.reservatieForm = new FormGroup({
       datum: new FormControl('', Validators.required),
       personen: new FormControl('', [
@@ -31,21 +37,27 @@ export class ReservatiesComponent implements OnInit {
         Validators.required,
       ]),
     });
-    this.id = Number(this.route.snapshot.paramMap.get('id'));
-    console.log(this.id);
-    this.getReservatiesByZaakId(this.id);
   }
 
   getReservatiesByZaakId(id) {
     this.reservatiesservice.getReservatiesByZaakId(id).subscribe((data) => {
       this.reservaties = data;
+      console.log(this.reservaties);
     });
   }
 
   getZaak() {
     this.zaakService.getZaak(this.id).subscribe((data) => {
       this.zaak = data;
+      console.log(this.zaak);
     });
+  }
+
+  aantaltafelsmetzelfdeaantalstoelen() {
+
+    for (let tafel of this.zaak.tafels) {
+      this.tafels.set(tafel.stoelen, this.tafels.get(tafel.stoelen) + 1);
+    }
   }
 
   get personen() {
