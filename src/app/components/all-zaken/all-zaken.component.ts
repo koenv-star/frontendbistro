d4448b7bb534071429a68e30bbbe7d94ac4e6c21
michael creelle <michael.creelle@telenet.be>
2020-11-10 11:30:03 +0100

    added rating

--- src/app/components/all-zaken/all-zaken.component.ts
+++ src/app/components/all-zaken/all-zaken.component.ts
@@ -14,7 +14,7 @@ export class AllZakenComponent implements OnInit {
   constructor(zaakservice:ZaakService) {
     this.zaken = new Array();
     zaakservice.getAllZakenForDisplay().subscribe(res => {res.forEach(x => {
-      this.zaken[this.zaken.length] = new Zaak(x.id, x.naam, x.text,x.imageURL,null,null,null,null,null,null,null,null,null);
+      this.zaken[this.zaken.length] = new Zaak(x.id, x.naam, x.text,x.imageURL,null,x.rating,null,null,null,null,null,null,null);
     })});
    }
 
